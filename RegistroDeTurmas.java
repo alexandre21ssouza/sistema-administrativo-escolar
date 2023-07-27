120f70f7a7e712d005a48a43737ae156ff5a2adf
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-07-27 01:11:31 -0300

    Inserindo pesquisa por nome de aluno(a) no método readAluno

--- RegistroDeTurmas.java
+++ RegistroDeTurmas.java
@@ -9,33 +9,38 @@ public class RegistroDeTurmas {
 	ArrayList<Aluno> listaAlunos = new ArrayList<>();
 
 	public static void main(String[] args) {
+
 		CrudAluno aluno = new CrudAluno();
 
 		JOptionPane.showMessageDialog(null, "Olá, Seja muito bem vindo ao Sistema de alunos!");
 
 		String opcaoDeAcao = "Iniciar";
-		
-		while(opcaoDeAcao.equals("Iniciar") || opcaoDeAcao.equals("Reiniciar")) {
-			
-			int numeroDeAcao = -1;
-			while(true) {
 
-			try {
-						opcaoDeAcao = JOptionPane.showInputDialog(null,
-						"Digite a opção desejada: \n" + "1-Cadastrar novo Aluno(a).\n"
-								+ "2-Visualizar Alunos cadastados.\n" + "3-Atualizar cadastro de Aluno(a).\n"
-								+ "4-Deletar Aluno cadastrado.\n" + "0-Sair do Programa.");
-				 numeroDeAcao = Integer.parseInt(opcaoDeAcao);
-				 if(numeroDeAcao > 4 || numeroDeAcao < 0) {
-					 JOptionPane.showMessageDialog(null, "Digite um número válido");
-				 } else {
-				 break;
-				 }
-				 
-			} catch(NumberFormatException numero) {
-				JOptionPane.showMessageDialog(null, "Digite uma opção válida!!");
-							
-			 }
+		while (opcaoDeAcao.equals("Iniciar") || opcaoDeAcao.equals("Reiniciar")) {
+
+			int numeroDeAcao = -1;
+
+			while (true) {
+
+				try {
+					opcaoDeAcao = JOptionPane.showInputDialog(null,
+									"Digite a opção desejada: \n" + "1-Cadastrar novo Aluno(a).\n"
+									+ "2-Visualizar Alunos cadastados.\n" + "3-Atualizar cadastro de Aluno(a).\n"
+									+ "4-Deletar Aluno cadastrado.\n" + "0-Sair do Programa.");
+					numeroDeAcao = Integer.parseInt(opcaoDeAcao);
+
+					if (numeroDeAcao > 4 || numeroDeAcao < 0) {
+						JOptionPane.showMessageDialog(null, "Digite um número válido");
+
+					} else {
+						break;
+
+					}
+
+				} catch (NumberFormatException numero) {
+					JOptionPane.showMessageDialog(null, "Digite uma opção válida!!");
+
+				}
 			}
 
 			switch (numeroDeAcao) {
@@ -61,14 +66,15 @@ public class RegistroDeTurmas {
 					"Confirmação", JOptionPane.YES_NO_OPTION);
 
 			if (refazerOperacao == JOptionPane.YES_OPTION) {
-				JOptionPane.showMessageDialog(null, "Reiniciando operação!");
+				JOptionPane.showMessageDialog(null, "Voltando ao Menu de Operações!");
 				opcaoDeAcao = "Reiniciar";
+
 			} else {
 				opcaoDeAcao = "Sair do Programa!";
 
 			}
 		}
-	}		
+	}
 }
 
 
